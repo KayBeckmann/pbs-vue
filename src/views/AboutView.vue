f62a440d3909810878d89f03e375096faf0b0b03
snuppedeluxe <snuppedeluxe@gmail.com>
2024-07-09 09:53:08 +0000

    add menu on AboutView.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,13 @@
 <template>
+  <menuComponent></menuComponent>
   <div class="about">
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script setup>
+import menuComponent from "@/components/menuComponent.vue";
+</script>
 
 <style>
 @media (min-width: 1024px) {
