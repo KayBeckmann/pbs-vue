b965c452c46ec5619e9f9ea5d772f15f9187bd44
snuppedeluxe <snuppedeluxe@gmail.com>
2024-07-09 09:52:11 +0000

    add store for account

---
+++ src/stores/account.ts
@@ -0,0 +1,34 @@
+import { defineStore } from "pinia";
+
+export const useCounterStore = defineStore({
+  id: "account",
+  state: () => ({
+    count: 0,
+    more: "viele Weitere Optionen",
+  }),
+  actions: {
+    increaseCount() {
+      this.count++;
+    },
+    decreaseCount() {
+      if (this.count > 0) {
+        this.count--;
+      }
+    },
+  },
+  getters: {
+    oddOrEven() {
+      if (this.count % 2 === 0) {
+        return "even";
+      }
+      return "odd";
+    },
+    oddOrEvenArrow: (state) => {
+      if (state.count % 2 === 0) {
+        return "even";
+      } else {
+        return "odd";
+      }
+    },
+  },
+});
