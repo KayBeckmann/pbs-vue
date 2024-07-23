2d86da4016b25145a99b4b1eebf53a202161069d
snuppedeluxe <snuppedeluxe@gmail.com>
2024-07-23 19:34:37 +0000

    generate first test für account management

---
+++ src/components/accountManagement.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1>AccountManagement</h1>
+    <input type="text" v-model="name" placeholder="Kontobezeichnung">
+    <input type="text" v-model="iban" placeholder="IBAN">
+    <input type="text" v-model="bic" placeholder="BIC">
+    <input type="number" v-model="startkapital" placeholder="Startkapital">
+    <button @click="addAccount" >Hinzufügen</button>
+</template>
+
+<script setup>
+import {Account} from "@/models/account"
+import {useAccountStore} from "@/stores/account"
+
+const accountStore = useAccountStore()
+
+let name = ""
+let iban = ""
+let bic = ""
+let startkapital = 0
+
+function addAccount(){
+    accountStore.addAccount({name, iban, bic, startkapital})
+}
+</script>
+
+<style scoped lang="scss">
+h1 {
+  color:red;
+}
+</style>
