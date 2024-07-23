aae41328b4d14e5cb4093249691baaa8bb0caa70
snuppedeluxe <snuppedeluxe@gmail.com>
2024-07-23 19:34:09 +0000

    Add AccountComponent

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,12 @@
 <template>
   <menuComponent></menuComponent>
+  <acountManagement></acountManagement>
 </template>
 
 <script setup lang="ts">
 import menuComponent from "@/components/menuComponent.vue";
+import acountManagement from "@/components/accountManagement.vue"
 </script>
 
 <style scoped lang="scss">
-h1 {
-  font-size: xx-large;
-}
 </style>
