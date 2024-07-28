5051f727440f48e714b042d3c6e2c4971bb0a6bf
snuppedeluxe <snuppedeluxe@gmail.com>
2024-07-28 20:02:44 +0000

    add balance to class

--- src/models/account.ts
+++ src/models/account.ts
@@ -3,14 +3,16 @@ export class Account {
     bank: string;
     iban: string;
     bic: string;
+    balance: number;
     open: boolean;
     show: boolean;
 
-    constructor(id:number, bank:string, iban:string, bic:string, open:boolean, show:boolean) {
+    constructor(id:number, bank:string, iban:string, bic:string, balance:number, open:boolean = true, show:boolean = true) {
       this.id = id;
       this.bank = bank;
       this.iban = iban;
       this.bic = bic;
+      this.balance = balance;
       this.open = open;
       this.show = show;
     }
