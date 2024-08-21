160e554d1c1384a4a05b16821943bfc2d64238e2
Kay <info@beckmann-md.de>
2024-08-21 12:25:53 +0200

    edit menu class. add footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,27 @@
 <template>
-  <menuComponent></menuComponent>
+  <div class="center">
+    <menuComponent></menuComponent>
+  </div>
   <div class="content">
     <acountManagement></acountManagement>
   </div>
+  <footerComponent></footerComponent>
 </template>
 
 <script setup lang="ts">
 import menuComponent from "@/components/menuComponent.vue";
-import acountManagement from "@/components/accountManagement.vue"
+import acountManagement from "@/components/accountManagement.vue";
+import footerComponent from "@/components/footerComponent.vue";
 </script>
 
 <style scoped lang="scss">
+.center {
+  display: flex;
+  justify-content: center;
+  left: auto;
+  right: auto;
+}
 .content {
-  margin-top: 90px;
+  margin-top: 85px;
 }
 </style>
