45367769c6d66b09fbed08d2bec450acbc909cfe
kay <info@beckmann-md.de>
2024-08-26 21:05:47 +0200

    add login view

---
+++ src/views/LoginView.vue
@@ -0,0 +1,21 @@
+<template>
+    <menuComponent></menuComponent>
+    <div class="about">
+      <h1>This is an login page</h1>
+    </div>
+  </template>
+  
+  <script setup>
+  import menuComponent from "@/components/menuComponent.vue";
+  </script>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
