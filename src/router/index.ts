496ee0138a225674a1ad58675ce4e706b80e839e
Kay <info@beckmann-md.de>
2024-08-30 13:11:51 +0200

    add auth to home route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { supabase } from "@/supabase";
 import HomeView from "@/views/HomeView.vue";
+
+let localUser;
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +11,7 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+      meta: { requiresAuth: true },
     },
     {
       path: "/about",
@@ -15,18 +19,33 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      component: () => import("@/views/AboutView.vue"),
     },
     {
       path: "/login",
       name: "login",
-      component: () => import("../views/LoginView.vue"),
+      component: () => import("@/views/LoginView.vue"),
     },
   ],
 });
 
 //getUser
+async function getUser(next) {
+  localUser = await supabase.auth.getSession();
+  if (localUser.data.session == null) {
+    next("/login");
+  } else {
+    next();
+  }
+}
 
 //auth requirements
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    getUser(next);
+  } else {
+    next();
+  }
+});
 
 export default router;
