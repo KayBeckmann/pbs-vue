ea7e9cb6b2501238b40e4aa9f1b9346d75612df8
Kay <info@beckmann-md.de>
2024-08-30 09:58:40 +0200

    create basic login-structure

---
+++ src/components/loginComponent.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="box">
+    <h1>Login</h1>
+    <div class="inputContainer">
+      <label for="email">Email: </label>
+      <input type="email" name="email" id="email" />
+    </div>
+    <div class="inputContainer">
+      <label for="password">Password: </label>
+      <input type="password" name="password" id="password" />
+    </div>
+
+    <div class="buttonContainer">
+      <button>Create Account</button>
+      <button>Login</button>
+      <button>See user</button>
+      <button>Logout</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+// connect inputs
+
+// create inputs
+
+// login
+
+// seeCurrentUser
+
+// logout
+</script>
+
+<style scoped lang="scss">
+.box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 80dvh;
+  width: 500px;
+  border: 2px solid lightgray;
+  border-radius: 15px;
+  gap: 15px;
+
+  h1 {
+    font-size: 3rem;
+  }
+}
+.inputContainer {
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+}
+
+.buttonContainer {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+}
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,21 +1,18 @@
 <template>
-    <menuComponent></menuComponent>
-    <div class="about">
-      <h1>This is an login page</h1>
-    </div>
-  </template>
-  
-  <script setup>
-  import menuComponent from "@/components/menuComponent.vue";
-  </script>
-  
-  <style>
-  @media (min-width: 1024px) {
-    .about {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
-    }
-  }
-  </style>
-  
+  <div class="content">
+    <loginComponent></loginComponent>
+  </div>
+</template>
+
+<script setup>
+import loginComponent from "@/components/loginComponent.vue";
+</script>
+
+<style>
+.content {
+  height: 100dvh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
